47b00f0ebe260d33d1c534ed21575d079ae6666f
Patrick Harris <patricknpharris@gmail.com>
2018-09-25 16:36:23 +0100

    .all refactored to pull data from database

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -1,10 +1,10 @@
+require 'pg'
+
 # this class holds static bookmarks
 class Bookmark
   def self.all
-    [
-      'http://www.makersacademy.com',
-      'http://www.destroyallsoftware.com',
-      'http://www.google.com'
-    ]
+    connection = PG.connect(dbname: 'bookmark_manager')
+    list = connection.exec 'SELECT * FROM bookmarks'
+    list.map { |bookmark| bookmark['url'] }
   end
 end
